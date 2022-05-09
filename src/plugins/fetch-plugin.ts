fe079fdcb14c628ba2b5c9f40ac5b71f53f1593f
Jonathan Alvares <jonthan.alvares@yahoo.com>
2022-05-09 15:26:42 +0530

    Cached the npm modules for faster load times

---
+++ src/plugins/fetch-plugin.ts
@@ -0,0 +1,70 @@
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
+import localForage from "localforage";
+
+const cachedFiles = localForage.createInstance({
+  name: "cachedFiles",
+});
+
+export const fetchPlugin = (inputCode: string) => {
+  return {
+    name: "fetch-plugin",
+    setup(build: esbuild.PluginBuild) {
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // Check if the file is cached already
+        const cachedFile = await cachedFiles.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedFile) {
+          return cachedFile;
+        }
+      });
+
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+                const style = document.createElement('style');
+                style.innerText = '${escaped}';
+                document.head.appendChild(style);
+            `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await cachedFiles.setItem(args.path, result);
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        await cachedFiles.setItem(args.path, result);
+
+        return result;
+      });
+    },
+  };
+};
