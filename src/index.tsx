04990493da74cadb93e1270608f9f6e11f829720
Jonathan Alvares <jonthan.alvares@yahoo.com>
2022-05-16 11:31:32 +0530

    Created reducers for the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom";
-import CodeCell from "./components/code-cell";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
-    <div>
-      {/* <CodeCell /> */}
-      <TextEditor />
-    </div>
+    // @ts-ignore
+    <Provider store={store}>
+      <div>
+        {/* <CodeCell /> */}
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
