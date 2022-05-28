4e5a12e552cd17bab0f4d0a363f34c9e3cbcd90f
Jonathan Alvares <jonthan.alvares@yahoo.com>
2022-05-29 00:18:52 +0530

    Added a local API

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,11 +16,24 @@ exports.serveCommand = void 0;
 const commander_1 = require("commander");
 const local_api_1 = require("local-api");
 const path_1 = __importDefault(require("path"));
+const isProduction = process.env.NODE_ENV === "production";
 exports.serveCommand = new commander_1.Command()
     .command("serve [filename]")
     .description("Open a file for editing")
     .option("--p, --port <number>", "Port to listen on", "4005")
-    .action((filename = "notebook.js", option) => {
-    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
-    (0, local_api_1.serve)(parseInt(option.port), path_1.default.basename(filename), dir);
-});
+    .action((filename = "notebook.js", option) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+        yield (0, local_api_1.serve)(parseInt(option.port), path_1.default.basename(filename), dir, !isProduction);
+        console.log(`Opened ${filename} for editing. Navigate to http://localhost:${option.port} to edit.`);
+    }
+    catch (e) {
+        if (e.code === "EADDRINUSE") {
+            console.error(`Port ${option.port} is already in use. Please choose another port.`);
+        }
+        else {
+            console.error(e);
+        }
+        process.exit(1);
+    }
+}));
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -2,11 +2,32 @@ import { Command } from "commander";
 import { serve } from "local-api";
 import path from "path";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("--p, --port <number>", "Port to listen on", "4005")
-  .action((filename = "notebook.js", option: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(option.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", option: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(
+        parseInt(option.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      );
+      console.log(
+        `Opened ${filename} for editing. Navigate to http://localhost:${option.port} to edit.`
+      );
+    } catch (e: any) {
+      if (e.code === "EADDRINUSE") {
+        console.error(
+          `Port ${option.port} is already in use. Please choose another port.`
+        );
+      } else {
+        console.error(e);
+      }
+      process.exit(1);
+    }
   });
